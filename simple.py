4e9c4131882030acb1108505fcf4591f1aa01316
Samarth <samarth212@gmail.com>
2024-03-17 15:34:12 -0400

    added exit function

--- simple.py
+++ simple.py
@@ -8,4 +8,14 @@ def chat(prompt):
         messages=[{"role": "user", "content": prompt}]
     )
 
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+
+        response = chat(user_input)
+        print("Chatbot: ", response)
     
